d73d89cbeca374aff162e5c85cf0d2210c4be710
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2025-12-23 15:04:54 +0900

    update gen codes

--- run.sh
+++ run.sh
@@ -5,18 +5,45 @@ export HF_TOKEN=""
 DATASET_PATH="amphora/suhak-variants"
 DATASET_SUBSET="suhak_pivot"
 PROMPT_TEMPLATE=$'Solve the given question and output the final answer in the following format: \\boxed{{N}}. \n\n{question}'
-N=4
-BATCH_SIZE=10
+N=32
+BATCH_SIZE=25
 
 MODELS=(
+  "openai/gpt-oss-120b"
   "openai/gpt-oss-20b"
-  "Qwen/Qwen3-30B-A3B-Instruct-2507"
+  "Qwen/Qwen3-30B-A3B-Thinking-2507"
+  "Qwen/Qwen3-4B-Thinking-2507"
 )
 
-OUTPUT_TAG="general_prompt"
+OUTPUT_TAG="original_question"
 for MODEL in "${MODELS[@]}"; do
   echo "Running ${MODEL}..."
-  python suhak_variants_solve.py \
+  python variants_solve.py \
+    --model_name "$MODEL" \
+    --dataset_path "$DATASET_PATH" \
+    --dataset_subset "$DATASET_SUBSET" \
+    --prompt_template "$PROMPT_TEMPLATE" \
+    --n "$N" \
+    --batch_size "$BATCH_SIZE" \
+    --output_tag "$OUTPUT_TAG"
+done
+
+DATASET_PATH="amphora/suhak-variants"
+DATASET_SUBSET="suhak_pivot"
+PROMPT_TEMPLATE=$'Solve the given question and output the final answer in the following format: \\boxed{{N}}. \n\n{variant question}'
+N=32
+
+MODELS=(
+  "openai/gpt-oss-120b"
+  "openai/gpt-oss-20b"
+  "Qwen/Qwen3-30B-A3B-Thinking-2507"
+  "Qwen/Qwen3-4B-Thinking-2507"
+)
+
+OUTPUT_TAG="variant_question"
+for MODEL in "${MODELS[@]}"; do
+  echo "Running ${MODEL}..."
+  python variants_solve.py \
     --model_name "$MODEL" \
     --dataset_path "$DATASET_PATH" \
     --dataset_subset "$DATASET_SUBSET" \
@@ -28,16 +55,16 @@ done
 
 
 PROMPT_TEMPLATE=$'{question} \n\n {solution_text}.\n\nRefer to the question-solution set provided above. Solve the provided question below and output the final answer in the following format: \\boxed{{N}}. \n\n {variant question}'
-N=4
+N=128
 MODELS=(
-  "openai/gpt-oss-20b"
-  "Qwen/Qwen3-30B-A3B-Instruct-2507"
+  "openai/gpt-oss-120b"
+  "Qwen/Qwen3-30B-A3B-Thinking-2507"
 )
 
-OUTPUT_TAG="variant_prompt"
+OUTPUT_TAG="variant_with_reference"
 for MODEL in "${MODELS[@]}"; do
   echo "Running ${MODEL}..."
-  python suhak_variants_solve.py \
+  python variants_solve.py \
     --model_name "$MODEL" \
     --dataset_path "$DATASET_PATH" \
     --dataset_subset "$DATASET_SUBSET" \
