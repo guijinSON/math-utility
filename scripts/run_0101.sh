ade9dd6f3bf16277f71e02ad7cc6cbac278baf39
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2025-12-31 03:33:34 +0900

    update scripts

--- scripts/run_0101.sh
+++ scripts/run_0101.sh
@@ -3,48 +3,19 @@
 export HF_TOKEN=""
 
 DATASET_PATH="amphora/suhak-variants"
-DATASET_SUBSET="suhak_main"
-PROMPT_TEMPLATE=$'Solve the given question and output the final answer in the following format: \\boxed{{N}}. \n\n{question}'
-N=1024
-BATCH_SIZE=25
+DATASET_SUBSET="real_math_new_pivot"
 
+
+
+PROMPT_TEMPLATE=$'{question} \n\n {solution_text}.\n\nRefer to the question-solution set provided above. Solve the provided question below and output the final answer in the following format: \\boxed{{N}}. \n\n {variant question}'
+N=64
+BATCH_SIZE=25
 MODELS=(
-  # "openai/gpt-oss-120b"
-#   "openai/gpt-oss-20b"
-  "Qwen/Qwen3-235B-A22B-Thinking-2507"
-  "Qwen/Qwen3-30B-A3B-Thinking-2507"
-#   "Qwen/Qwen3-4B-Thinking-2507"
+     "Qwen/Qwen3-235B-A22B-Thinking-2507"
+      "Qwen/Qwen3-30B-A3B-Thinking-2507"
 )
 
-OUTPUT_TAG="suhak_original_question"
-for MODEL in "${MODELS[@]}"; do
-  echo "Running ${MODEL}..."
-  python variants_solve.py \
-    --model_name "$MODEL" \
-    --dataset_path "$DATASET_PATH" \
-    --dataset_subset "$DATASET_SUBSET" \
-    --prompt_template "$PROMPT_TEMPLATE" \
-    --n "$N" \
-    --batch_size "$BATCH_SIZE" \
-    --output_tag "$OUTPUT_TAG"
-done
-
-DATASET_PATH="amphora/suhak-variants"
-DATASET_SUBSET="suhak_main"
-PROMPT_TEMPLATE=$'Solve the given question and output the final answer in the following format: \\boxed{{N}}. \n\n{variant question}'
-BATCH_SIZE=25
-N=1024
-
-MODELS=(
-  # "openai/gpt-oss-120b"
-   "Qwen/Qwen3-235B-A22B-Thinking-2507"
-  "Qwen/Qwen3-30B-A3B-Thinking-2507"
-  # "openai/gpt-oss-20b"
-  # "Qwen/Qwen3-30B-A3B-Thinking-2507"
-  # "Qwen/Qwen3-4B-Thinking-2507"
-)
-
-OUTPUT_TAG="suhak_variant_question"
+OUTPUT_TAG="real_math_new_pivot_variant_with_reference"
 for MODEL in "${MODELS[@]}"; do
   echo "Running ${MODEL}..."
   python variants_solve.py \
@@ -58,9 +29,6 @@ for MODEL in "${MODELS[@]}"; do
 done
 
 
-
-DATASET_PATH="amphora/suhak-variants"
-DATASET_SUBSET="suhak_pivot"
 
 PROMPT_TEMPLATE=$"""You are an impartial mathematical judge.
 You will be given a math problem and a proposed solution.
@@ -114,7 +82,7 @@ MODELS=(
   "Qwen/Qwen3-30B-A3B-Thinking-2507"
 )
 
-OUTPUT_TAG="suhak_llm_judge_pointwise"
+OUTPUT_TAG="real_math_new_pivot_llm_judge_pointwise"
 for MODEL in "${MODELS[@]}"; do
   echo "Running ${MODEL}..."
   python variants_solve.py \
@@ -177,7 +145,7 @@ MODELS=(
   "Qwen/Qwen3-30B-A3B-Thinking-2507"
 )
 
-OUTPUT_TAG="llm_judge_pairwise"
+OUTPUT_TAG="real_math_new_pivot_llm_judge_pairwise"
 for MODEL in "${MODELS[@]}"; do
   echo "Running ${MODEL}..."
   python variants_solve.py \
@@ -190,24 +158,3 @@ for MODEL in "${MODELS[@]}"; do
     --output_tag "$OUTPUT_TAG"
 done
 
-
-PROMPT_TEMPLATE=$'{question} \n\n {solution_text}.\n\nRefer to the question-solution set provided above. Solve the provided question below and output the final answer in the following format: \\boxed{{N}}. \n\n {variant question}'
-N=64
-BATCH_SIZE=25
-MODELS=(
-   "Qwen/Qwen3-235B-A22B-Thinking-2507"
-  # "Qwen/Qwen3-30B-A3B-Thinking-2507"
-)
-
-OUTPUT_TAG="variant_with_reference"
-for MODEL in "${MODELS[@]}"; do
-  echo "Running ${MODEL}..."
-  python variants_solve.py \
-    --model_name "$MODEL" \
-    --dataset_path "$DATASET_PATH" \
-    --dataset_subset "$DATASET_SUBSET" \
-    --prompt_template "$PROMPT_TEMPLATE" \
-    --n "$N" \
-    --batch_size "$BATCH_SIZE" \
-    --output_tag "$OUTPUT_TAG"
-done
